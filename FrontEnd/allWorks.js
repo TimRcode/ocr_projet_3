836b6903383eaafbcaf42512c9321a1c0169c5df
timr <timrcode0@gmail.com>
2023-01-27 08:56:19 +0100

    add categories

--- FrontEnd/allWorks.js
+++ FrontEnd/allWorks.js
@@ -1,14 +1,68 @@
+//stocker l'API
 const apiWorks = "http://localhost:5678/api/works"
+
+const apiCategories = "http://localhost:5678/api/categories"
+
 
 async function getWorks(){
 
+//recupère l'API
     const response =  await fetch(apiWorks)
     const datas = await response.json()
 
-    const gallery = document.querySelector(".gallery")
+    //boucle pour afficher les éléments de l'API(Works)
+    for(let data of datas){
 
-    for(let data of datas)
-    console.log(gallery)
+        const gallery = document.querySelector(".gallery")
+
+        const figure = document.createElement('figure')
+        
+        const img = document.createElement('img')
+        img.src = data.imageUrl
+        img.setAttribute("crossorigin", "anonymous")
+
+        const figCaption = document.createElement('figcaption')
+        figCaption.innerText = data.title
+        
+        gallery.appendChild(figure)
+
+        figure.appendChild(img)
+        figure.  appendChild(figCaption)     
+        
+    }
+
 }
 
 getWorks()
+
+
+async function getCategories(){
+
+    const response = await fetch(apiCategories)
+    const datas = await response.json() 
+
+    const ul = document.createElement("ul")
+
+    const all = document.createElement("li")
+    all.innerText = "Tous"
+
+
+        for(let categorie of datas){
+            const categories = document.querySelector(".categories")
+        
+
+        
+        const li = document.createElement("li")
+        li.innerText = categorie.name
+            
+        categories.appendChild(ul)
+        ul.appendChild(all)
+        ul.appendChild(li)
+        
+    }
+ }
+
+getCategories()
+
+
+a
