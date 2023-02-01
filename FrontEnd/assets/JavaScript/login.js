f22ec291f2c72230ba6a51d69d2fccc4992a9c70
timr <timrcode0@gmail.com>
2023-02-01 17:46:14 +0100

    fix login

--- FrontEnd/assets/JavaScript/login.js
+++ FrontEnd/assets/JavaScript/login.js
@@ -1,32 +1,59 @@
 
+const login = document.getElementById('login');
+const email = document.getElementById('name');
+const password = document.getElementById('pass');
 
 
 
 
 
+function sendLoginFormDatas() {
+    let user = {
+        email: email.value,
+        password: password.value,
+    };
 
-
-
-    const userPass = {
-    email : document.querySelector("[name=email]"),
-    password : document.querySelector("[name=password]")
+    postLoginForm(user);
 }
+    
 
+function postLoginForm(user) {
+    
+        fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(user),
+        })
+            .then(response => {
+                // Si l'email ou le mot de passe est incorrect
+                if (!response.ok) {
+                    displayMessage('error', "&times; Erreur dans l'identifiant ou le mot de passe", loginFormEl);
+                    emailInput.focus();
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                if (data.userId) connecting(data);
+            })
+            .catch(error => {
+                throw new Error(error);
+            });
+    };
 
+login.addEventListener('submit', event => {
+    event.preventDefault();
+    sendLoginFormDatas();
+});
 
-const chargeUtile = JSON.stringify(userPass);
+function connection(data) {
+    
+    localStorage.setItem('userId', data.userId);
+    localStorage.setItem('token', data.token);
 
-const reponse =  await fetch  ("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: chargeUtile
-})
-
-const data =  await reponse.json()
-const token = data.token
-console.log(token)
-
-
-
-
-
+    
+    const homePage = window.location + '../../index.html';
+    window.location.replace(homePage);
+}
