f0a5c937a5451665e968f51694d58e5bbc6ef805
timr <timrcode0@gmail.com>
2023-02-15 21:13:14 +0100

    Fix delete works

--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -1,113 +1,100 @@
+import { getWorks } from './works.js';
+const Auth = JSON.parse(localStorage.getItem("Auth"));
 
-import {getWorks} from './works.js';
-let works = await getWorks()
-const Auth = JSON.parse(localStorage.getItem("Auth"))
+const publishButton = document.querySelector(".publier");
+const btnModal = document.querySelector("#modal-mod");
+const modal = document.querySelector('#modal');
+const gallery = document.querySelector(".gallery-modal");
 
+let works = [];
+let selectedWorks = [];
 
+async function refreshWorks() {
+  works = await getWorks();
+  selectedWorks = [];
+  gallery.innerHTML = "";
+  for (const work of works) {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.setAttribute('class', 'image-wrapper');
 
-const publishButton = document.querySelector(".publier")
+    const img = document.createElement('img');
+    img.src = work.imageUrl;
+    img.setAttribute("crossorigin", "anonymous");
 
-const btnModal = document.querySelector("#modal-mod")
-const modal =document.querySelector('#modal')
-const gallery = document.querySelector(".gallery-modal")
+    const bin = document.createElement('img');
+    bin.setAttribute('class', 'bin');
+    bin.src = "assets/images/bin.png";
+    bin.setAttribute("crossorigin", "anonymous");
 
-if(Auth && Auth.token){
-btnModal.addEventListener("click", event =>{
-    event.preventDefault
-    //la modal apparaît
-    modal.style.display = "";
-    //La galerie s'affiche q'une fois à chaque clic
-    gallery.innerHTML =""
-      //boucle pour afficher tous les works
-      for(let work of works){
-      
-      const imageWrapper = document.createElement('div')
-      imageWrapper.setAttribute('class', 'image-wrapper')
+    const enlarge = document.createElement('img');
+    enlarge.setAttribute('class', 'enlarge');
+    enlarge.src = "assets/images/Move.Png";
 
-      const img = document.createElement('img')  
-      img.src = work.imageUrl
-      img.setAttribute("crossorigin", "anonymous")
+    const editor = document.createElement('span');
+    editor.innerHTML = "éditer";
 
-      const bin = document.createElement('img')
-      bin.setAttribute('class', 'bin')
-      bin.src ="assets/images/bin.png"
-      bin.setAttribute("crossorigin", "anonymous")
+    imageWrapper.appendChild(img);
+    imageWrapper.appendChild(bin);
+    imageWrapper.appendChild(editor);
+    imageWrapper.appendChild(enlarge);
+    gallery.appendChild(imageWrapper);
 
-      const enlarge = document.createElement('img')
-      enlarge.setAttribute('class', 'enlarge')
-      enlarge.src = "assets/images/Move.Png"
-
-      const editor = document.createElement('span')
-      editor.innerHTML ="éditer"
-
-      gallery.appendChild(imageWrapper)
-
-      imageWrapper.appendChild(img)
-      imageWrapper.appendChild(bin)
-      imageWrapper.appendChild(editor)
-      imageWrapper.appendChild(enlarge)
-/* requete delete
-      bin.addEventListener("click", async function() {
-        // Suppression de l'imageWrapper parent
-        imageWrapper.remove();
-      
-        // Envoi d'une requête DELETE pour supprimer l'image
-        const response = await fetch(`http://localhost:5678/api/works/${work.id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${Auth.token}`
-          }
-        });
-      
-        // Vérification de la réponse du serveur
-        if (!response.ok) {
-          throw new Error("Impossible de supprimer l'image");
-        }
-      
-        console.log("Image supprimée avec succès");
-      });
-
-
-      document.querySelector(".remove-gallery")
-      .addEventListener("click", function() {
-        // Suppression de l'imageWrapper parent
-        imageWrapper.remove();
-      });
-
-      */
-      }
-})  
+    bin.addEventListener("click", event => {
+      event.preventDefault()
+      selectedWorks.push(work.id);
+      imageWrapper.remove();
+    });
+  }
 }
+
+if (Auth && Auth.token) {
+  btnModal.addEventListener("click", event => {
+    event.preventDefault();
+    modal.style.display = "";
+    refreshWorks();
+  });
+
+  publishButton.addEventListener("click", async function (event){
+        event.preventDefault()
+
+    for (const workId of selectedWorks) {
+      const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${Auth.token}`
+        }
+      });
+      if (response.ok) {
+        console.log(`Travail ${workId} supprimé`);
+      } else {
+        console.error(`Erreur lors de la suppression du travail ${workId}`);
+      }
+    }
+    closeModal();
+  });
+}
+
 function closeModal() {
   modal.style.display = "none";
-
 }
 
 const closeButton = document.querySelector("#modal .close");
-closeButton.addEventListener("click", function() {
-  closeModal();
+closeButton.addEventListener("click", event=>{
+  event.preventDefault()
+  closeModal()
 });
 
 document.addEventListener("keydown", function(event) {
+  event.preventDefault()
   if (event.key === "Escape") {
     closeModal();
   }
 });
 
-
 modal.addEventListener("click", function(event) {
+  event.preventDefault()
   if (event.target === modal) {
     closeModal();
   }
 });
-
-
-//modal 2
-modaleAdd = document.querySelector("#modale2")
-const btnAdd = document.querySelector(".add-image")
-
-btnAdd.addEventListener("click", event=>{
-  event.preventDefault
-  modaleAdd.style.display=""
-})
